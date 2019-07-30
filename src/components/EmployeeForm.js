981e7644a22d2323bc93cdb82b0893bd54f74aa9
Alif Jahan <alif@interconnectionbd.com>
2019-07-30 18:31:03 +0600

    Updated onChange with single method

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -8,13 +8,13 @@ class EmployeeForm extends React.Component {
             name: '',
             age: '',
         };
-        this.onNameChange = this.onNameChange.bind(this);
+        this.onChangeHandler = this.onChangeHandler.bind(this);
         this.onAgeChange = this.onAgeChange.bind(this);
     }
 
-    onNameChange(event){
+    onChangeHandler(event){
         this.setState({
-            name: event.target.value
+            [event.target.name]: event.target.value
         });
     }
 
@@ -29,12 +29,12 @@ class EmployeeForm extends React.Component {
             <form>
                 <div className="form-group">
                     <label htmlFor="exampleFormControlInput1">Name</label>
-                    <input onChange={(event) => this.onNameChange(event)} value={this.state.name} autoComplete="off" name="name" type="text" className="form-control" id="exampleFormControlInput1" placeholder="Alif Jahan" />
+                    <input onChange={(event) => this.onChangeHandler(event)} value={this.state.name} autoComplete="off" name="name" type="text" className="form-control" id="exampleFormControlInput1" placeholder="Alif Jahan" />
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="exampleFormControlInput2">Age</label>
-                    <input min="0" max="100" onChange={(event) => this.onAgeChange(event)} value={this.state.age} autoComplete="off" name="age" type="number" className="form-control" id="exampleFormControlInput2" placeholder="You age" />
+                    <input min="0" max="100" onChange={(event) => this.onChangeHandler(event)} value={this.state.age} autoComplete="off" name="age" type="number" className="form-control" id="exampleFormControlInput2" placeholder="You age" />
                 </div>
 
                 <button onClick={(event) => this.props.submitThis(event, this.state.name, this.state.age)} type="button" className="btn btn-success btn-lg">{this.props.currentButtonName}</button>
