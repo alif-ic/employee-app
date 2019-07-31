16c3fcd7e44b5c8b8a4892d570d2c93b8d87113f
Alif Jahan <alif@interconnectionbd.com>
2019-07-31 18:33:27 +0600

    Fixed Employee

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -24,7 +24,9 @@ class Nav extends React.Component {
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
                     <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li className="nav-item active">
-                        <a className="nav-link"> <Link to="/homes" /><span className="sr-only">(current)</span></a>
+                        <a className="nav-link">
+                            
+                        <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" to="/contacts">Contacts</a>
